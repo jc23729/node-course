45f8e0fde012b1a06b9cdb23c50d0f093217410a
Juan Cruz <jc23729@gmail.com>
2021-02-15 17:18:50 -0500

    update

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,4 @@
-//require function is how we load things in/ modules
-const fs = require('fs')
+const name = 'Andrew'
 
-//fs.writefileSync take in two arguments both are strings, name of file, and then the data to write
-fs .writeFileSync('notes.txt', 'My name is John.')
+console.log(name)
+

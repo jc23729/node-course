3ff2c32c6cce7a62f50f2861dd49ca03c6064bbd
Juan Cruz <jc23729@gmail.com>
2021-02-15 22:52:42 -0500

    nodemon globally

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,16 +1,20 @@
-const validator = require("validator")
+// const validator = require("validator")
 
 const getNotes = require('./notes.js');
 
 const msg = getNotes()
 
+const chalk = require('chalk');
+
 // console.log(validator.isEmail('example.com'));
 
-console.log(validator.isUrl('http://google.com'));
+// console.log(validator.isUrl('http://google.com'));
+
+const greenMsg = chalk.red('Error')
+console.log(greenMsg)
 
 
-
-
+// or console.log(chalk.green('Success!'));
 
 
 

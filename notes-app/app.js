53d22a217bae6850f88bc68f604db6fbe251ea54
Juan Cruz <jc23729@gmail.com>
2021-02-16 22:03:53 -0500

    create add, remove command

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,5 +2,26 @@ const chalk = require('chalk');
 const yargs = require('yargs');
 const getNotes = require('./notes.js');
 
+//customize yargs version
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function handler() {
+        console.log('Adding a new note')
+
+    }
+})
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function handler() {
+        console.log('Removing a note')
+    }
+})
+
+
 
 console.log(yargs.argv)

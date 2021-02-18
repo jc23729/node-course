882362d77147a2a256cb740679beb98d5bdd35b4
Juan Cruz <jc23729@gmail.com>
2021-02-18 15:43:36 -0500

    update

--- notes-app/playground/1-json.js
+++ notes-app/playground/1-json.js
@@ -8,3 +8,9 @@ const fs = require('fs');
 
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
+
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data = JSON.parse(dataJSON)
+
+// console.log(data.title)

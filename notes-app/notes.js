7702a9832273b5c1678817d89a148b2096a09b21
Juan Cruz <jc23729@gmail.com>
2021-02-18 20:43:33 -0500

    update

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,8 +1,66 @@
+const fs = require('fs')
+
+
 // Create getNotes function that returns "Your notes..."
 
 
 const getNotes = function(){
     return 'your notes....';
 }
+//dont have loadNotes file yet...
+const addNote = function(title, body){
+    const notes = loadNotes()
+    //gonna push on an object with title and body properties
+    //gonna use array filter method to check for duplicate notes
+    // if return true, keep array or false
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
 
-module.exports = getNotes
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added')
+    } else {
+        console.log('Note title taken')
+    }
+
+
+}
+// going to take an argument, its gonna take an array of notes
+const saveNotes = function(notes){
+    //stringify the data
+    const dataJSON = JSON.stringify(notes)
+    //save it to the file system, we're writing whatever is in that dataJSON file
+    fs.writeFileSync('notes.json', dataJSON)
+
+}
+
+const loadNotes = function(){
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    // if it catches an error = e it will return an empty array
+    }catch(e){
+        return []
+    }
+
+}
+
+// Macs-MBP:notes-app mac$ node app.js add --title="t" --body= "b"
+// { _: [ 'add', 'b' ], title: 't', body: '', '$0': 'app.js' }
+// Macs-MBP:notes-app mac$ node app.js add --title="t" --body="b"
+// { _: [ 'add' ], title: 't', body: 'b', '$0': 'app.js' }
+// Macs-MBP:notes-app mac$ node app.js add --title="List" --body="Sweater, Pants" 
+// { _: [ 'add' ], title: 'List', body: 'Sweater, Pants', '$0': 'app.js' }
+// Macs-MBP:notes-app mac$ 
+
+
+module.exports = {
+    getNotes : getNotes,
+    addNote : addNote
+}

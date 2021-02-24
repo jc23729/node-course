8218fad4fb54b6dcc70780b6c23659b10ee2de5d
Juan Cruz <jc23729@gmail.com>
2021-02-23 23:30:13 -0500

    update

--- notes-app/playground/2-arrow-function.js
+++ notes-app/playground/2-arrow-function.js
@@ -1,23 +1,23 @@
-// const square = function (x){
-//     return x * x;
+// // const square = function (x){
+// //     return x * x;
+// // }
+
+// // const square = (x) => {
+// //     x * x;
+// // }
+// // Console.log(square(2));
+
+
+// const event = {
+//     name: 'Birthday Party',
+//     guestList: ['Andrew', 'Jen','Mike']
+//     printGuestList: function printGuestList(str) {
+//         console.log('Guest list for' + this.name);
+
+//         this.guestList.forEach((guest) => {
+//             console.log(guest + ' is attending ' + this.name)
+//     })
 // }
-
-// const square = (x) => {
-//     x * x;
 // }
-// Console.log(square(2));
-
-
-const event = {
-    name: 'Birthday Party',
-    guestList: ['Andrew', 'Jen','Mike']
-    printGuestList: function printGuestList(str) {
-        console.log('Guest list for' + this.name);
-
-        this.guestList.forEach((guest) => {
-            console.log(guest + ' is attending ' + this.name)
-    })
-}
-}
-event.printGuestList()
-//checking to see if this is working
+// event.printGuestList()
+// //checking to see if this is working

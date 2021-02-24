0fb8c350ee37ae2a5df3db5b2413f4b434dbc943
Juan Cruz <jc23729@gmail.com>
2021-02-23 23:11:26 -0500

    update

---
+++ notes-app/playground/2-arrow-function.js
@@ -0,0 +1,17 @@
+// const square = function (x){
+//     return x * x;
+// }
+
+// const square = (x) => {
+//     x * x;
+// }
+// Console.log(square(2));
+
+
+// const event = {
+//     name: 'Birthday Party',
+//     guestList: ['Andrew', 'Jen','Mike']
+//     printGuestList: function printGuestList(str) {
+//         console.log('Guest list for' + this.name);
+//     }
+// }
